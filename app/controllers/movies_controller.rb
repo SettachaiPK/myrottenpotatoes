65915f8d79a59ec08443f6b8667522cb8f2b5ecd
chayanuttan <looktanclub2008@hotmail.com>
2020-10-19 20:29:01 -0700

    searchadd

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   before_action :check_for_cancel, :only => [:create, :update]
-  before_action :check_for_login, :only => [:search_tmdb, :all_destroy, :new, :edit, :destroy]
+  before_action :check_for_login, :only => [:search_tmdb,  :new, :edit, :destroy]
 
   def check_for_cancel
     if params[:commit] == "Cancel"
@@ -12,6 +12,13 @@ class MoviesController < ApplicationController
     unless set_current_user
       flash[:warning] = "Please log in before do this action"
       redirect_to movies_path
+    end
+  end
+  
+  def add
+    if Movie.exists?(:title => params.title,:description => params.overview) == false
+      permitted = {:title => params.title,:release_date =>params.release_date,:description => params.overview}
+      Movie.create!(permitted)
     end
   end
 
@@ -52,15 +59,6 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  def createall_from_movies(search_movie)
-      search_movie.each do |movie|
-        if Movie.exists?(:title => movie.title,:description => movie.overview) == false
-          permitted = {:title => movie.title,:release_date =>movie.release_date,:description => movie.overview}
-          Movie.create!(permitted)
-        end
-      end
-    end  
-
     def search_tmdb
       #@search_params = params[:search_terms]
       #@search_params = " " if @search_params  == ""
@@ -75,18 +73,11 @@ class MoviesController < ApplicationController
       @search_params = params[:search_terms]
       @search_params = " " if @search_params  == ""
       @search = Tmdb::Movie.find(@search_params)
-      createall_from_movies(@search)
       if @search != []
         render "search"
       else
         flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
         redirect_to movies_path
       end
-    end
-
-    def all_destroy
-      Movie.destroy_all
-      flash[:notice] = "All Movie deleted."
-      redirect_to movies_path
     end
 end
